130d234fa7acdb8dec3d5ff00328b1dcfdeed658
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-08 22:58:26 +0100

    made function playround work

--- javascript.js
+++ javascript.js
@@ -1,7 +1,33 @@
-const choices = ["rock", "paper", "scissors"];
+const computerChoices = ["ROCK", "PAPER", "SCISSORS"];
 
 function getComputerChoice(list) {
     return list[Math.floor((Math.random()*list.length))];
 }
 
-console.log(getComputerChoice(choices))
+let answer = "scissors"
+const playerSelection = answer.toUpperCase()
+
+const computerSelection = getComputerChoice(computerChoices)
+
+console.log("You play: " + playerSelection)
+console.log("Computer plays: " + computerSelection)
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return console.log("It's a draw!")
+    } else if ((playerSelection === "ROCK" && computerSelection === "PAPER") 
+            || (playerSelection === "PAPER" && computerSelection === "SCISSORS") 
+            || (playerSelection === "SCISSORS" && computerSelection === "ROCK")){
+        return console.log("You lose!")
+
+    } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") 
+            || (playerSelection === "PAPER" && computerSelection === "ROCK")
+            || (playerSelection === "SCISSORS" && computerSelection === "PAPER")){
+        return console.log("You win!")
+    } else {
+        return console.log("Wrong choice, try again")
+    }
+        
+}
+
+playRound(playerSelection, computerSelection)
