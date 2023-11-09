825b15d3dfdf985cc7e4e61549a671c80d8e8572
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-09 07:52:51 +0100

    made points to be allocated

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 function game(playRound) {
+   let playerPoints = 0
+   let computerPoints = 0
 const computerChoices = ["ROCK", "PAPER", "SCISSORS"];
 
 function getComputerChoice(list) {
@@ -15,20 +17,24 @@ console.log("Computer plays: " + computerSelection)
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
-        return console.log("It's a draw!")
+        // return console.log("It's a draw!")
+
     } else if ((playerSelection === "ROCK" && computerSelection === "PAPER") 
             || (playerSelection === "PAPER" && computerSelection === "SCISSORS") 
             || (playerSelection === "SCISSORS" && computerSelection === "ROCK")){
-        return console.log("You lose!")
+        // return console.log("You lose!")
+        return playerPoints++
 
     } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") 
             || (playerSelection === "PAPER" && computerSelection === "ROCK")
             || (playerSelection === "SCISSORS" && computerSelection === "PAPER")){
-        return console.log("You win!")
+        // return console.log("You win!")
+        return computerPoints++
+
     } else {
         return console.log("Wrong choice, try again")
     }
-        
+        console.log("Score")
 }
     playRound(playerSelection, computerSelection) 
     }
