3a4e907634a72f4e98297cd66da9a095e67f9694
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-09 11:11:22 +0100

    minor aesthetyc changes

--- javascript.js
+++ javascript.js
@@ -20,27 +20,26 @@ console.log("Computer plays: " + computerSelection)
 
 function playRound(playerSelection, computerSelection) {
  if (playerSelection === computerSelection){
-     return console.log("It's a draw!"), round++
+     return console.log("It's a draw!\n---------------"), round++
 
  } else if ((playerSelection === "ROCK" && computerSelection === "PAPER") 
          || (playerSelection === "PAPER" && computerSelection === "SCISSORS") 
          || (playerSelection === "SCISSORS" && computerSelection === "ROCK")){
-        return console.log(computerSelection + " beats " + playerSelection), computerPoints++, round++
+        return console.log(computerSelection + " beats " + playerSelection + "\n---------------"), computerPoints++, round++
         
 
  } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") 
          || (playerSelection === "PAPER" && computerSelection === "ROCK")
          || (playerSelection === "SCISSORS" && computerSelection === "PAPER")){
-        return console.log(playerSelection + " beats " + computerSelection), playerPoints++, round++
+        return console.log(playerSelection + " beats " + computerSelection + "\n---------------"), playerPoints++, round++
        
 
  } else {
-     return console.log("Wrong choice, try again")
+     return console.log("Wrong choice, try again\n---------------")
  }
 
 }
  playRound(playerSelection, computerSelection)
-     
  }
 
  for (i= 0; i< 5; i++)
@@ -53,4 +52,4 @@ function playRound(playerSelection, computerSelection) {
  } else if (playerPoints == computerPoints){
     console.log("It's a draw")
  }
- console.log("Press F5 to play again!")
+ console.log("Press F5 to play again.")
