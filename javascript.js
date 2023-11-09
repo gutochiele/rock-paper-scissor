78b090d853c7afb1074befe3d301fa2639af774b
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-09 08:56:03 +0100

    Update javascript.js
    added point system to the game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 function game(playRound) {
    let playerPoints = 0
    let computerPoints = 0
+   let round = 0
 const computerChoices = ["ROCK", "PAPER", "SCISSORS"];
 
 function getComputerChoice(list) {
@@ -17,24 +18,24 @@ console.log("Computer plays: " + computerSelection)
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
-        // return console.log("It's a draw!")
+        return console.log("It's a draw!"), round++
 
     } else if ((playerSelection === "ROCK" && computerSelection === "PAPER") 
             || (playerSelection === "PAPER" && computerSelection === "SCISSORS") 
             || (playerSelection === "SCISSORS" && computerSelection === "ROCK")){
         // return console.log("You lose!")
-        return playerPoints++
+        return playerPoints++, round++
 
     } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") 
             || (playerSelection === "PAPER" && computerSelection === "ROCK")
             || (playerSelection === "SCISSORS" && computerSelection === "PAPER")){
         // return console.log("You win!")
-        return computerPoints++
+        return computerPoints++, round++
 
     } else {
         return console.log("Wrong choice, try again")
     }
-        console.log("Score")
+        console.log("Round: " + round\n"Score: Player " + playerPoints + " x " computerPoints + "Computer)
 }
     playRound(playerSelection, computerSelection) 
     }
