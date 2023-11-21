bf1bb21bacbedfb1c3171997f2dce98eabefda25
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-21 21:48:28 +0100

    starting to create UI

--- javascript.js
+++ javascript.js
@@ -8,14 +8,14 @@ function playRound(playerSelec, compSelec) {
    if (playerSelec === compSelec){
       return console.log("It's a draw."), round++;
 
-   } else if ((playerSelec === "ROCK" && compSelec === "PAPER")
-            || (playerSelec === "PAPER" && compSelec === "SCISSORS")
-            || (playerSelec === "SCISSORS" && compSelec === "ROCK")){
+   } else if ((playerSelec === "rock" && compSelec === "paper")
+            || (playerSelec === "paper" && compSelec === "scissors")
+            || (playerSelec === "scissors" && compSelec === "rock")){
          return console.log(compSelec + " beats " + playerSelec), computerPoints++, round++;
             
-   } else if ((playerSelec === "ROCK" && compSelec === "SCISSORS") 
-            || (playerSelec === "PAPER" && compSelec === "ROCK")
-            || (playerSelec === "SCISSORS" && compSelec === "PAPER")){
+   } else if ((playerSelec === "rock" && compSelec === "scissors") 
+            || (playerSelec === "paper" && compSelec === "rock")
+            || (playerSelec === "scissors" && compSelec === "paper")){
          return console.log(playerSelec + " beats " + compSelec), playerPoints++, round++;
          
    } else {
@@ -23,44 +23,49 @@ function playRound(playerSelec, compSelec) {
    }
 };
 
-//CREATE THE GAME FUNC AND LOOPS IT FOR 5 ROUNDS
-function game() {
-   for ( ; round < 5; ){
 
    //GETS RANDOM INPUT FROM THE COMPUTER
-   const computerChoices = ["ROCK", "PAPER", "SCISSORS"];
+   const computerChoices = ["rock", "paper", "scissors"];
    function getComputerChoice(list) {
    return list[Math.floor((Math.random()*list.length))];
    }
    let compSelec;
    compSelec = getComputerChoice(computerChoices);
 
-   //GETS CASE INSENSITIVE INPUT FROM THE PLAYER
-   let playerSelec = prompt("Rock, paper or scissors?\nChose wisely:");
-   playerSelec = playerSelec.toUpperCase();
 
-   //PRINTS SELECTED INPUT FROM BOTH PLAYERS
-   console.log("You play: " + playerSelec);
-   console.log("Computer plays: " + compSelec);
+   const btns = document.querySelectorAll('button');
+   btns.forEach((button) => {
+   button.addEventListener('click', playRound);
+
+})
+
+  
+
+   //GETS CASE INSENSITIVE INPUT FROM THE PLAYER      
+   // let playerSelec = prompt("Rock, paper or scissors?\nChose wisely:");
+   // playerSelec = playerSelec.toUpperCase();
+
+   // //PRINTS SELECTED INPUT FROM BOTH PLAYERS
+   // console.log("You play: " + playerSelec);
+   // console.log("Computer plays: " + compSelec);
 
   
    //CALLS THE playRound FUNC TO START INSIDE THE game FUNC
-   playRound(playerSelec, compSelec);
-   console.log("Round: " + round)
-   console.log("------------------");
-   };
-};
+   // playRound(playerSelec, compSelec);
+   // console.log("Round: " + round)
+   // console.log("------------------");
+   // };
+// };
    
 //CALLS FOR THE GAME FUNCTION TO START
- game();
+//  game();
 
  //DISPLAYS SCORE AND GAME STATS BY THE END OF THE MATCH
- console.log("Score: Player " + playerPoints + " x " + computerPoints + " Computer");
- if (playerPoints > computerPoints){
-    console.log("You WIN! :D")
- } else if (playerPoints < computerPoints){
-    console.log("You lost! :(")
- } else if (playerPoints === computerPoints){
-    console.log("It's a draw!")
- }
- console.log("Press F5 to play again.");
+//  console.log("Score: Player " + playerPoints + " x " + computerPoints + " Computer");
+//  if (playerPoints > computerPoints){
+//     console.log("You WIN! :D")
+//  } else if (playerPoints < computerPoints){
+//     console.log("You lost! :(")
+//  } else if (playerPoints === computerPoints){
+//     console.log("It's a draw!")
+//  }
