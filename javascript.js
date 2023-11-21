f1d4f8c9bb85b82055784ca6a24ac36b0f315c5d
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-21 22:07:29 +0100

    added the eventlisteners that play the game

--- javascript.js
+++ javascript.js
@@ -2,47 +2,63 @@
 let playerPoints = 0;
 let computerPoints = 0;
 let round = 0;
+let lose = 'You lost.'
+let win = 'You win.'
 
 //CREATES A FUNCTION THAT PLAYS EACH ROUND AND SETS THE GAME RULES
 function playRound(playerSelec, compSelec) {
    if (playerSelec === compSelec){
       return console.log("It's a draw."), round++;
 
-   } else if ((playerSelec === "rock" && compSelec === "paper")
-            || (playerSelec === "paper" && compSelec === "scissors")
-            || (playerSelec === "scissors" && compSelec === "rock")){
-         return console.log(compSelec + " beats " + playerSelec), computerPoints++, round++;
+   } else if ((playerSelec === "Rock" && compSelec === "Paper")
+            || (playerSelec === "Paper" && compSelec === "Scissors")
+            || (playerSelec === "Scissors" && compSelec === "Rock")){
+         return console.log(compSelec + " beats " + playerSelec + `. ${lose}`), computerPoints++, round++;
             
-   } else if ((playerSelec === "rock" && compSelec === "scissors") 
-            || (playerSelec === "paper" && compSelec === "rock")
-            || (playerSelec === "scissors" && compSelec === "paper")){
-         return console.log(playerSelec + " beats " + compSelec), playerPoints++, round++;
+   } else if ((playerSelec === "Rock" && compSelec === "Scissors") 
+            || (playerSelec === "Paper" && compSelec === "Rock")
+            || (playerSelec === "Scissors" && compSelec === "Paper")){
+         return console.log(playerSelec + " beats " + compSelec + `. ${win}`), playerPoints++, round++;
          
    } else {
       return console.log("I did't understand that, try again");
    }
 };
 
-
    //GETS RANDOM INPUT FROM THE COMPUTER
-   const computerChoices = ["rock", "paper", "scissors"];
+   const computerChoices = ["Rock", "Paper", "Scissors"];
    function getComputerChoice(list) {
    return list[Math.floor((Math.random()*list.length))];
    }
-   let compSelec;
-   compSelec = getComputerChoice(computerChoices);
+   // let compSelec;
+   // compSelec = getComputerChoice(computerChoices);
 
 
-   const btns = document.querySelectorAll('button');
-   btns.forEach((button) => {
-   button.addEventListener('click', playRound);
-
-})
-
+   const RockButton = document.querySelector('.rock');
+   const PaperButton = document.querySelector('.paper');
+   const ScissorsButton = document.querySelector('.scissors');
   
 
+   RockButton.addEventListener('click', () => {
+      const compSelec = getComputerChoice(computerChoices);
+      const playerSelec = 'Rock'
+      playRound(playerSelec, compSelec)
+   });
+
+   PaperButton.addEventListener('click', () => {
+      const compSelec = getComputerChoice(computerChoices);
+      const playerSelec = 'Paper'
+      playRound(playerSelec, compSelec)
+   });
+   
+   ScissorsButton.addEventListener('click', () => {
+      const compSelec = getComputerChoice(computerChoices);
+      const playerSelec = 'Scissors'
+      playRound(playerSelec, compSelec)
+   });
+
    //GETS CASE INSENSITIVE INPUT FROM THE PLAYER      
-   // let playerSelec = prompt("Rock, paper or scissors?\nChose wisely:");
+   // let playerSelec = prompt("Rock, Paper or Scissors?\nChose wisely:");
    // playerSelec = playerSelec.toUpperCase();
 
    // //PRINTS SELECTED INPUT FROM BOTH PLAYERS
