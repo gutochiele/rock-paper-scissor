1717371f608dd14451271688cb93e0be6e6899e4
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-23 22:20:40 +0100

    completed the game with basic interface

--- javascript.js
+++ javascript.js
@@ -2,29 +2,87 @@
 let playerPoints = 0;
 let computerPoints = 0;
 let round = 0;
-let lose = 'You lost.'
-let win = 'You win.'
+
+//SELECT THE ELEMENTS
+const RockButton = document.querySelector('.rock');
+const PaperButton = document.querySelector('.paper');
+const ScissorsButton = document.querySelector('.scissors');
+const outcomeDiv = document.querySelector('.outcome');
+const matchWinner = document.querySelector('.winner');
+const buttons = document.querySelectorAll('button');
+
+//CREATE THE EVENT LISTENERS OF THE BUTTONS
+RockButton.addEventListener('click', () => {
+   const compSelec = getComputerChoice(computerChoices);
+   const playerSelec = 'Rock'
+   playRound(playerSelec, compSelec)
+   roundWinner(playerPoints, computerPoints) 
+});
+
+PaperButton.addEventListener('click', () => {
+   const compSelec = getComputerChoice(computerChoices);
+   const playerSelec = 'Paper'
+   playRound(playerSelec, compSelec)
+   roundWinner(playerPoints, computerPoints) 
+});
+
+ScissorsButton.addEventListener('click', () => {
+   const compSelec = getComputerChoice(computerChoices);
+   const playerSelec = 'Scissors'
+   playRound(playerSelec, compSelec)
+   roundWinner(playerPoints, computerPoints)   
+});
 
 //CREATES A FUNCTION THAT PLAYS EACH ROUND AND SETS THE GAME RULES
 function playRound(playerSelec, compSelec) {
    if (playerSelec === compSelec){
-      return console.log("It's a draw."), round++;
+      const p = document.createElement('p');
+      p.innerText = `It's a draw, you both picked ${playerSelec}`
+      outcomeDiv.appendChild(p)
+      round++
 
    } else if ((playerSelec === "Rock" && compSelec === "Paper")
             || (playerSelec === "Paper" && compSelec === "Scissors")
             || (playerSelec === "Scissors" && compSelec === "Rock")){
-         return console.log(compSelec + " beats " + playerSelec + `. ${lose}`), computerPoints++, round++;
+         const p = document.createElement('p');
+         p.innerText = `${compSelec} beats ${playerSelec}, you lose.`;
+         outcomeDiv.appendChild(p)
+         computerPoints++, round++;
             
    } else if ((playerSelec === "Rock" && compSelec === "Scissors") 
             || (playerSelec === "Paper" && compSelec === "Rock")
             || (playerSelec === "Scissors" && compSelec === "Paper")){
-         return console.log(playerSelec + " beats " + compSelec + `. ${win}`), playerPoints++, round++;
+         const p = document.createElement('p');
+         p.innerText = `${playerSelec} beats ${compSelec}, you win.`;
+         outcomeDiv.appendChild(p)
+         playerPoints++, round++
          
    } else {
-      return console.log("I did't understand that, try again");
+      return alert("I did't understand that, try again");
    }
 };
 
+
+function roundWinner(playerPoints, computerPoints) {
+   
+   if (playerPoints == 5) {
+      const h2 = document.createElement('h2');
+      h2.innerText = `You are the winner, it's ${playerPoints} to ${computerPoints} for you! \nPress F5 to play again.`;
+      h2.style.color = 'red';
+      matchWinner.appendChild(h2);
+      buttons.forEach(button => button.disabled = true);
+      
+   } else if (computerPoints == 5) {
+      const h2 = document.createElement('h2');
+      h2.innerText = `You are the loser! It's ${computerPoints} to ${playerPoints} for the computer. \nPress F5 to play again.`;
+      h2.style.color = 'red';
+      matchWinner.appendChild(h2);
+      buttons.forEach(btn => btn.disabled = true);
+      
+
+}};
+
+   
    //GETS RANDOM INPUT FROM THE COMPUTER
    const computerChoices = ["Rock", "Paper", "Scissors"];
    function getComputerChoice(list) {
@@ -34,28 +92,7 @@ function playRound(playerSelec, compSelec) {
    // compSelec = getComputerChoice(computerChoices);
 
 
-   const RockButton = document.querySelector('.rock');
-   const PaperButton = document.querySelector('.paper');
-   const ScissorsButton = document.querySelector('.scissors');
-  
 
-   RockButton.addEventListener('click', () => {
-      const compSelec = getComputerChoice(computerChoices);
-      const playerSelec = 'Rock'
-      playRound(playerSelec, compSelec)
-   });
-
-   PaperButton.addEventListener('click', () => {
-      const compSelec = getComputerChoice(computerChoices);
-      const playerSelec = 'Paper'
-      playRound(playerSelec, compSelec)
-   });
-   
-   ScissorsButton.addEventListener('click', () => {
-      const compSelec = getComputerChoice(computerChoices);
-      const playerSelec = 'Scissors'
-      playRound(playerSelec, compSelec)
-   });
 
    //GETS CASE INSENSITIVE INPUT FROM THE PLAYER      
    // let playerSelec = prompt("Rock, Paper or Scissors?\nChose wisely:");
@@ -76,7 +113,7 @@ function playRound(playerSelec, compSelec) {
 //CALLS FOR THE GAME FUNCTION TO START
 //  game();
 
- //DISPLAYS SCORE AND GAME STATS BY THE END OF THE MATCH
+//  DISPLAYS SCORE AND GAME STATS BY THE END OF THE MATCH
 //  console.log("Score: Player " + playerPoints + " x " + computerPoints + " Computer");
 //  if (playerPoints > computerPoints){
 //     console.log("You WIN! :D")
